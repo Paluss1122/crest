a3d9f8342a5c88359fa284ad61999d8418169a6e
Dale Eidd <dale@eidd.name>
2021-09-24 14:56:36 -0700

    Add displacement and camera ray correction to ocean position
    This probably has a bunch of negative knock on effects.

--- crest/Assets/Crest/Crest/Scripts/OceanRenderer.cs
+++ crest/Assets/Crest/Crest/Scripts/OceanRenderer.cs
@@ -132,6 +132,11 @@ namespace Crest
         internal bool _hasTeleportedThisFrame = false;
         Vector3 _oldViewerPosition = Vector3.zero;
 
+        [SerializeField]
+        bool _displacementCorrection;
+        [SerializeField]
+        bool _detailAtCameraRay;
+
         public Transform Root { get; private set; }
 
         // does not respond to _timeProvider changing in inspector
@@ -416,6 +421,10 @@ namespace Crest
         float _viewerHeightAboveWaterSmooth = 0f;
 
         SampleHeightHelper _sampleHeightHelper = new SampleHeightHelper();
+
+        SampleHeightHelper _displacementCorrectionHelper = new SampleHeightHelper();
+
+        RayTraceHelper _detailAtCameraRayHelper = new RayTraceHelper(50f, 2f);
 
         public static OceanRenderer Instance { get; private set; }
 
@@ -1151,6 +1160,13 @@ namespace Crest
         void LateUpdatePosition()
         {
             var pos = Viewpoint.position;
+
+            _detailAtCameraRayHelper.Init(Viewpoint.position, Viewpoint.forward);
+            if (_detailAtCameraRayHelper.Trace(out float dist) && _detailAtCameraRay) pos = Viewpoint.position + Viewpoint.forward * dist;
+
+            _displacementCorrectionHelper.Init(ViewCamera.transform.position, 0f, true);
+            _displacementCorrectionHelper.Sample(out Vector3 position, out _, out _);
+            if (_displacementCorrection) pos = new Vector3(pos.x - position.x, pos.y, pos.z - position.z);
 
             // maintain y coordinate - sea level
             pos.y = Root.position.y;
